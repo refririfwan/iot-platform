72eba24de44fe88674c0afc0e96788c8b69ca104
Refri Rifwan <refririfwan@gmail.com>
2022-12-06 15:24:08 +0700

    Completed Function Signup - Add dynamodb configurations - Put user data to dynamodb when signup - Add nanoid for id using require ( version 3 still support)

--- user/signup.js
+++ user/signup.js
@@ -1,7 +1,9 @@
+const { nanoid } = require('nanoid');
 const AWS = require('aws-sdk')
 const sendResponse = require('../utils/response')
 const validateInput = require('../utils/validation')
 const cognito = new AWS.CognitoIdentityServiceProvider()
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
 module.exports.handler = async (event) => {
     try {
@@ -25,7 +27,7 @@ module.exports.handler = async (event) => {
                 }],
             MessageAction: 'SUPPRESS'
         }
-        const response = await cognito.adminCreateUser(params).promise();
+        const response = await cognito.adminCreateUser(params).promise()
         if (response.User) {
             const paramsForSetPass = {
                 Password: password,
@@ -35,7 +37,49 @@ module.exports.handler = async (event) => {
             };
             await cognito.adminSetUserPassword(paramsForSetPass).promise()
         }
-        return sendResponse(200, { message: 'User registration successful' })
+        const userId = nanoid()
+        const timestamp = new Date().toISOString()
+
+        // create params data user for dynamodb
+        const userParams = {
+          TableName: process.env.DYNAMODB_TABLE,
+          Item: {
+            id: 'email#'+email,
+            metadata: 'user#'+ userId,
+            email: email,
+            user_id: userId,
+            created_at: timestamp,
+            updated_at: '',
+          },
+        }
+
+        // put item data user in dynamodb
+        await dynamoDb.put(userParams).promise()
+
+        // create params data user detail for dynamodb
+        const userDetailParams = {
+          TableName: process.env.DYNAMODB_TABLE,
+          Item: {
+            id: 'user#'+ userId,
+            metadata: 'email#'+ email,
+            user_id: userId,
+            email: email,
+            created_at: timestamp,
+            updated_at: '',
+            name: '',
+            phone_number: '',
+            address: '',
+            city: '',
+            province: '',
+            country: '',
+            zip_code: '',
+          },
+        }
+
+        // put item data user detail in dynamodb
+        await dynamoDb.put(userDetailParams).promise()
+
+        return sendResponse(201, { message: 'User registration successful' })
     }
     catch (error) {
         const message = error.message ? error.message : 'Internal server error'
